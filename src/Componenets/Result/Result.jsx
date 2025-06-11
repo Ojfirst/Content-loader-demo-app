cf46fb78ba8ac19afcb400bf94770a1823f5b7cf
Simon <omojide.o.j@gmail.com>
2025-06-11 17:14:23 +0100

    Add Card.jsx and card.module.css

--- src/Componenets/Result/Result.jsx
+++ src/Componenets/Result/Result.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
+import classes from './Result.module.css';
 
 const Result = (props) => {
-  const listItem = props.processedData ? props.processedData.map((item) => (
-    <li key={item.id}>
-      {item.name} ({item.age} years old)
-    </li>
-  )) : null;
+	const listItem = props.processedData
+		? props.processedData.map((item) => (
+				<li key={item.id}>
+					{item.name} ({item.age} years old)
+				</li>
+		  ))
+		: null;
 
 	return (
 		<div>
-			<ul>
+			<ul className={classes.ul}>
 				{listItem}
-        {!listItem.length && <li>No data available</li>}
-       
+				{!listItem.length && <li>No data available</li>}
 			</ul>
 		</div>
 	);
